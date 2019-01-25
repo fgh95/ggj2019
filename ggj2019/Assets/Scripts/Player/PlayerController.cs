62e16aeadb583047a42c9ed3b8e5b3fc7c48c80d
forsterjoel@gmail.com <forsterjoel@gmail.com>
2019-01-25 22:55:27 +0000

    added basic player controller

---
+++ ggj2019/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float playerMaxSpeed = 20.0f;
+	private Rigidbody playerRigidBody;
+	private Vector3 playerDx;
+	
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidBody = GetComponent<Rigidbody>();
+        playerDx = new Vector3(0.0f,0.0f,0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+    	playerDx = new Vector3(0.0f,0.0f,0.0f);
+    	if (Input.GetKey ("w")) {
+            playerDx.z += Time.deltaTime;
+        }
+        if (Input.GetKey ("s")) {
+            playerDx.z -= Time.deltaTime;
+        }
+        if (Input.GetKey ("d")) {
+            playerDx.x += Time.deltaTime;
+        }
+        if (Input.GetKey ("a")) {
+            playerDx.x -= Time.deltaTime;
+        }
+
+        playerDx = Vector3.Normalize(playerDx);
+        playerDx = playerDx*playerMaxSpeed;
+
+        playerDx = Quaternion.LookRotation(transform.forward) * playerDx;
+
+        playerRigidBody.velocity = playerDx;
+    }
+}

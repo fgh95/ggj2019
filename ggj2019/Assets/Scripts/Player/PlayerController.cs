145eec211a28fcacfa0085180b0f800ff84e36ba
forsterjoel@gmail.com <forsterjoel@gmail.com>
2019-01-26 13:05:44 +0000

    added ramps to stairs, rebaked navmesh

--- ggj2019/Assets/Scripts/Player/PlayerController.cs
+++ ggj2019/Assets/Scripts/Player/PlayerController.cs
@@ -44,8 +44,7 @@ public class PlayerController : MonoBehaviour
 
         //Enforce sticking to the floor (may need to change if we use stairs and shit)
         playerDx = Vector3.ProjectOnPlane(playerDx, Vector3.up);
-        playerRigidBody.velocity = new Vector3(playerDx.x,0.0f,playerDx.z);
-        playerRigidBody.velocity = playerDx;
+        playerRigidBody.velocity = new Vector3(playerDx.x,playerRigidBody.velocity.y,playerDx.z);
 
     }
 }

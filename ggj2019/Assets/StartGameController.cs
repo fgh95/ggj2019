1cdb67b4b884ae533c025c94934555d5741c2d63
forsterjoel@gmail.com <forsterjoel@gmail.com>
2019-01-27 09:48:45 +0000

    menu

---
+++ ggj2019/Assets/StartGameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartGameController : MonoBehaviour
+{
+	public Button startButton;
+	public AudioSource ping;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(startGame);
+    }
+
+    void startGame(){
+    	ping.Play();
+    	SceneManager.LoadScene("SampleScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
